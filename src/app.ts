06b1dc69a553c96fafc5d73eef1192bca878a9d9
mkmahmud <mahmudulhasanmk434@gmail.com>
2023-05-29 01:27:32 +0600

    setup cors and parser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 
 // import express = require('express')
-import express from "express";
-const app  = express()
-const port =   5000;
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app : Application = express()
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Cors
+app.use(cors())
+
+// Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
+// Test Route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working ')
 })
 
 export default app
