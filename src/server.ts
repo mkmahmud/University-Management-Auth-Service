2b4805213ab359bbce1e75bdc48eaa8e93bb0427
mkmahmud <mahmudulhasanmk434@gmail.com>
2023-05-29 20:46:11 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,19 @@
 import mongoose from "mongoose";
 import config from "./config";
-import app from './app';
-
+import app from "./app";
 
 async function Main() {
-    try {
-        await mongoose.connect(config.database_url as string)
-        console.log('Databse connected')
-        app.listen(config.port, () => {
-            console.log(`University management system listening on port ${config.port}`)
-        })
-
-    }
-    catch (err) {
-        console.log('Connection Failed')
-    }
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Databse connected");
+    app.listen(config.port, () => {
+      console.log(
+        `University management system listening on port ${config.port}`
+      );
+    });
+  } catch (err) {
+    console.log("Mongo Connection Failed");
+  }
 }
-
-
-
 
 Main();
