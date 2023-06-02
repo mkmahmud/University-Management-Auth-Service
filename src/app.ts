2e341bf68f6597523632800472bb0b5ee49d99a5
mkmahmud <mahmudulhasanmk434@gmail.com>
2023-06-02 22:14:01 +0600

    skipping hooks

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
-
 // import express = require('express')
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app : Application = express()
-const port = 5000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 // Cors
 app.use(cors())
 
 // Parser
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 
 // Test Route
 app.get('/', (req: Request, res: Response) => {
